2bbb209657f711129ddb894fa612760ab89d6d11
samurai456 <127340863+samurai456@users.noreply.github.com>
2024-01-18 21:25:05 +0500

    Update main.tsx

--- test_task/src/main.tsx
+++ test_task/src/main.tsx
@@ -8,11 +8,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
         <Provider store={store}>
             <BrowserRouter>
                 <App />
             </BrowserRouter>
         </Provider>
-    </React.StrictMode>,
 )
